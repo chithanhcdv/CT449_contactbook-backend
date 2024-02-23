2808acf1508eac8e84bd186092a5e35d51f2ff1b
chithanhcdv <chithanhcdv456@gmail.com>
2024-02-23 21:57:30 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,9 @@
 const config = {
     app: {
-        port: process.env.PORT || 3000,
+        port: process.env.PORT || 3000 // Sử dụng biến môi trường PORT nếu có, nếu không sử dụng cổng 3000 mặc định
+    },
+    db: {
+        uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/contactbook"
     }
 };
 
