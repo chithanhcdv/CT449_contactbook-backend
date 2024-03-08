d0b4c04ef08808340816484e50e864dc6359bb0f
chithanhcdv <chithanhcdv456@gmail.com>
2024-03-08 15:35:28 +0700

    Cai dat cac handler truy xuat CSDL

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -15,7 +15,7 @@ class ContactService {
         };
 
         // Remove undefined fields
-        Objects.keys(contact).forEach(
+        Object.keys(contact).forEach(
             (key) => contact[key] === undefined && delete contact[key]
         );
         return contact;
